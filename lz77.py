78e94c89fd33b4c7b542cb2d9f14358d78bb900d
Boris Skurikhin <boriskurikhin@gmail.com>
2020-12-14 23:07:10 -0500

    working lz77?

--- lz77.py
+++ lz77.py
@@ -2,34 +2,47 @@
 # find the longest match in the running window
 def findLongestMatch(searchSpace: bytearray, matchBytes: bytearray) -> tuple:
     matchLength = len(matchBytes)
+    searchSize = len(searchSpace)
     while matchLength > 0:
         lookup = matchBytes[:matchLength]
         if lookup in searchSpace:
-            return searchSpace.index(lookup), matchLength
+            return searchSize - searchSpace.rindex(lookup), matchLength
         matchLength -= 1
     return 0, 0
-    
-def lz77 (inp: bytearray) -> bytearray:
-    index, output, SEARCH_SIZE, LOOKAHEAD_SIZE = 0, bytearray(), 4095, 15
+
+def lz77rev (inp: bytearray) -> bytearray:
+    length, index = len(inp), 0
+    output, j = b'', 0
+    for i in range(0, length, 3):
+        relativePos      = int(inp[i] | ((inp[i+1] & 0x0F) << 8))
+        matchLength      = int((inp[i + 1] & 0xF0) >> 4)
+        nextCharacter    = inp[i + 2] & 0xFF
+        if matchLength:
+            output += output[j-relativePos:j-relativePos+matchLength]
+            j += matchLength
+        elif nextCharacter:
+            output += nextCharacter.to_bytes(1, byteorder='little')
+            j += 1
+    return output
+
+def lz77 (inp: bytes) -> bytes:
+    index, output, SEARCH_SIZE, LOOKAHEAD_SIZE = 0, bytes(), 4095, 15
 
     while index < len(inp):
         searchStart = max(0, index - SEARCH_SIZE)
         lookaheadEnd = min(len(inp), index + LOOKAHEAD_SIZE)
-
         # run the matching algorithm
-        matchIndex, matchLength = findLongestMatch(inp[searchStart: index], inp[index: lookaheadEnd])
+        relativePos, matchLength = findLongestMatch(inp[searchStart: index], inp[index: lookaheadEnd])
         # relative offset
-        relativePos = index - matchIndex
-        # next character
-        nextChar = 0 if index + matchIndex >= len(inp) else inp[index + matchIndex]
-        # pack into 3 bytes
-        output.append(0x000000FF & relativePos) # 8 bits of relative position
-        output.append((0x00000F00 & relativePos) >> 8 | (0x0000000F & matchLength) << 4) # 4 bits of position and 4 bits of length
-        output.append(0xFFFFFFFF & nextChar) # 8 bits of next character
+        nextChar = 0 if index + matchLength >= len(inp) else inp[index + matchLength]
+
+        output += (0xFF & relativePos).to_bytes(1, byteorder='little') # 8 bits of relative position
+        output += ((0xF00 & relativePos) >> 8 | (0xF & matchLength) << 4).to_bytes(1, byteorder='little') # 4 bits of position and 4 bits of length
+        output += nextChar.to_bytes(1, byteorder='little') # 8 bits of next character
         index += max(1, matchLength)
     return output    
 
 
-k = lz77(bytearray('hello hello', 'utf-8'))
+k = lz77(bytes('hello hello', 'utf-8'))
 print(k)
-# print(unlz77(k))
+print(lz77rev(k))
